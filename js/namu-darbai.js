e4f760e645072199e7b5a22bfabb0bc7eaa80814
borntobeprogrammer <mindaugasg.vilnius@gmail.com>
2022-12-14 13:48:27 +0200

    if

---
+++ js/namu-darbai.js
@@ -0,0 +1,51 @@
+let money = 10;
+console.log(money);
+
+money = 50;
+console.log(money);
+
+money = 150;
+console.log(money);
+
+const hello = 'hello';
+console.log(hello);
+
+const girls = "girls";
+console.log(girls);
+
+const sentence = 'hello girls';
+console.log(sentence);
+
+const a1 = [10, 2, 8, 4, 6];
+
+let aSum = 0;
+aSum += a1[0];
+aSum += a1[1];
+aSum += a1[2];
+aSum += a1[3];
+aSum += a1[4];
+console.log('Pirmo:', aSum);
+
+const b = [8, 12, 5, 7, 1];
+console.log(b);
+const c = [2, 4, 9, 3, 5];
+console.log(c);
+
+const usersNames = ['Lietuva', 'yra', 'geriausia', 'salis', 'pasaulyje'];
+console.log(usersNames);
+
+const abc = ['s', 'v', 'e', 'i', 'k', 'a', 's'];
+
+const separator = '-';
+const ending = '.';
+let abcString = 'sveikas: ';
+
+abcString += abc[0] + separator;
+abcString += abc[1] + separator;
+abcString += abc[2] + separator;
+abcString += abc[3] + separator;
+abcString += abc[4] + separator;
+abcString += abc[5] + separator;
+abcString += abc[6] + ending;
+
+console.log(abcString);
