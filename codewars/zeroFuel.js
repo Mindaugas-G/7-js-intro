31641b1f29c2bff8c5ca4701839f24c12f1f74cb
borntobeprogrammer <mindaugasg.vilnius@gmail.com>
2022-12-29 19:15:48 +0200

    Student marks average

--- codewars/zeroFuel.js
+++ codewars/zeroFuel.js
@@ -8,3 +8,26 @@ function zeroFuel(distanceToPump, mpg, fuelLeft) {
 console.log(zeroFuel(50, 25, 2), '-->', true);
 console.log(zeroFuel(100, 50, 1), '-->', false);
 console.log(zeroFuel(40, 10, 10), '-->', true);
+
+
+
+var summation = function (num) {
+    let suma = 0;
+  for (let i=1; i< num; i++) {
+    //0+1= 1
+    //1+2 =3
+    //3+3 = 6
+    //6 + 4 = 10
+    //10 + 5 = 15
+    //15 + 6 = 21
+    //21 + 7 =28
+    //28 + 8 = 36
+    suma = suma + i
+  }
+    if(num >= 1){
+       return suma
+    }
+    return 1
+    
+  
+  }
